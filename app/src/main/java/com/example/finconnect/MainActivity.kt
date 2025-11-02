bfa11a897a74e1aff41bd77b80bc14bcfa823cd7
Abubakar <abubakarmunir15@gmail.com>
2025-11-02 00:46:29 -0600

    initial project setup

---
+++ app/src/main/java/com/example/finconnect/MainActivity.kt
@@ -0,0 +1,69 @@
+package com.example.finconnect
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.finconnect.ui.theme.FinConnectTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            FinConnectTheme {
+                /*Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Abu",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }*/
+                TestComposeScreen()
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Composable
+fun TestComposeScreen() {
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        Text(
+            text = "Jetpack Compose is working ✅",
+            color = Color.Green
+        )
+    }
+}
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    FinConnectTheme {
+        Greeting("Android")
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun PreviewTestComposeScreen() {
+    TestComposeScreen()
+}
