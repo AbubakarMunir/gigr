87307700a99c52c25b4a5c53232e9c58982ee0ec
Abubakar <abubakarmunir15@gmail.com>
2025-11-10 23:13:40 -0700

    project name updations warnings fixed

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,7 +11,7 @@ android {
     }
 
     defaultConfig {
-        applicationId = "com.example.finconnect"
+        applicationId = "com.example.gigr"
         minSdk = 24
         targetSdk = 36
         versionCode = 1
--- app/src/main/java/com/example/gigr/MainActivity.kt
+++ app/src/main/java/com/example/gigr/MainActivity.kt
@@ -5,14 +5,13 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import com.example.gigr.ui.navigation.AppNavigation
-import com.example.gigr.ui.theme.FinConnectTheme
-
+import com.example.gigr.ui.theme.gigrTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            FinConnectTheme {
+            gigrTheme {
                 AppNavigation()
             }
         }
