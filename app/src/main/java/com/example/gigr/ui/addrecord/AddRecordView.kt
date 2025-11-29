4f0e29c55ea2d2f4f4ea2e9fd9c8c7c6a59f06ec
Abubakar <abubakarmunir15@gmail.com>
2025-11-29 00:51:33 -0700

    basic structure for add record view

--- app/src/main/java/com/example/gigr/ui/addrecord/AddRecordView.kt
+++ app/src/main/java/com/example/gigr/ui/addrecord/AddRecordView.kt
@@ -1,42 +1,39 @@
 package com.example.gigr.ui.addrecord
 
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
-import com.example.gigr.ui.AppButton
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun AddRecordScreen(navController: NavController) {
-    Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .padding(16.dp),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        AppButton(
-            text = "Add Record",
-            onClick = {  },
-            containerColor = Color.Blue
-        )
-        Spacer(modifier = Modifier.height(16.dp))
-        AppButton(
-            text = "Back",
-            onClick = { navController.popBackStack() },
-            containerColor = Color.Gray
-        )
+    Scaffold(
+        topBar ={
+            TopAppBar(title={Text(" Add Record")},
+                navigationIcon = {IconButton(onClick = {navController.popBackStack()}) {
+                    Icon(Icons.Filled.ArrowBack, contentDescription = "Back")
+                }
+                }
+            )
+        }
+    )
+    {
+            paddingvalues -> Column(modifier = Modifier.fillMaxSize().padding(paddingvalues)) { }
     }
+
 }
 
 @Preview(showBackground = true)
